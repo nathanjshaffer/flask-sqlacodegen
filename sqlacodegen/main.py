2a04d10ef03856a2991e7a4b236af6c70d879e98
nathan <nathanjshaffer@gmail.com>
2024-11-08 21:26:11 -0500

    use fully qualified table names

--- sqlacodegen/main.py
+++ sqlacodegen/main.py
@@ -58,7 +58,7 @@ def main():
 
     engine = create_engine(args.url)
     import_dialect_specificities(engine)
-    metadata = MetaData()
+    metadata = MetaData(schema=default_schema)
     tables = args.tables.split(',') if args.tables else None
     ignore_cols = args.ignore_cols.split(',') if args.ignore_cols else None
     metadata.reflect(engine, args.schema, not args.noviews, tables)
